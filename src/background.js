4e45aa6917367e1d8422e72ea39aa27a3ea3b39d
SamanthaLLee <samantha.lin.lee@gmail.com>
2021-01-15 18:28:28 -0500

    Update background.js
    removed brackets

--- src/background.js
+++ src/background.js
@@ -8,5 +8,4 @@ chrome.runtime.onInstalled.addListener(function() {
 		// Set up timer
 		// Every x seconds, do one of the actions
 		// We can also send a message to the content script from here to execute the functions in that file
-  });
 });
