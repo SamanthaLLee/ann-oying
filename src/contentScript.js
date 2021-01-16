5683c271e61ba405af3d1e0c8713adb641af0fbc
Nabihah01 <73140311+Nabihah01@users.noreply.github.com>
2021-01-15 22:46:37 -0500

    Update contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,15 @@
 // Functions for each inconvenience 
 
-var changeHTML = function() {
-	
+var changeFontSize = function() {
+	chrome.fontSettings.setDefaultFontSize({pixelSize: 150}, function())
 }
+
+var changeFontStyle = function() {
+	chrome.tabs.query({currentWindow: true}, function(tabs){
+		chrome.fontSettings.setFont( { genericFamily: 'cursive', script: 'Nkgb', fontId: 'MS PGothic' } );
+	});
+}
+	
+changeFontStyle()
 
 // Listeners
