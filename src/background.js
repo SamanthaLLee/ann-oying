be91a2cf701fd1a3e37074ae2cdabced7cf95795
Justine C <47696696+adorkable17@users.noreply.github.com>
2021-01-16 14:04:09 -0500

    added popup test
    Brought over necessary changes to test features from main branch. Have been running into errors while testing so still need to test original features & popup. Will do so at next team meeting.

--- src/background.js
+++ src/background.js
@@ -1,22 +1,29 @@
 // Functions for each inconvenience 
 var rickRoll = function() {
-	chrome.tabs.create("https://www.youtube.com/watch?v=DLzxrzFCyOs", function(){})
+	chrome.tabs.create("https://www.youtube.com/watch?v=DLzxrzFCyOs")
+	console.log("rickRoll success");
 }
 
 var duplicate = function(){
 	chrome.tabs.query({active: true}, function(tabs){ 
-		max = tabs.length - 1;
-		rng = Math.random() * (max - 1) + 1;
+		var max = tabs.length - 1;
+		var rng = Math.random() * (max - 1) + 1;
 		chrome.tabs.duplicate(rng);
-	});
+	})
+	console.log("duplicate success");
 }
 
 var openTopSites = function(){
-	chrome.topSites.get(sitesToOpen[], function(){}){
-		max = sitesToOpen.length - 1;
-		rng = Math.random() * (max - 1) + 1;
+	chrome.topSites.get(sitesToOpen, function(){
+		var max = sitesToOpen.length - 1;
+		var rng = Math.random() * (max - 1) + 1;
 		chrome.tabs.create(sitesToOpen[rng].url);
-	}
+	})
+	console.log("openTopSites success");
+}
+
+var annePopsUp = function (){
+	chrome.action.setPopUp("Testing 1, 2, 3 :)", function(){})
 }
 
 var deleteWindow = function() {
@@ -31,7 +38,6 @@ var muteTab = function() {
 		}
 	});
 }
-
 // Listeners
 
 // On installation, this function will execute
@@ -39,4 +45,35 @@ chrome.runtime.onInstalled.addListener(function() {
 		// Set up timer
 		// Every x seconds, do one of the actions
 		// We can also send a message to the content script from here to execute the functions in that file
+
+var changeFontSize = function() {
+	//chrome.fontSettings.setDefaultFontSize({pixelSize: 120}, function(){});
+	console.log("changeFontSize");
+}
+
+var changeFontStyle = function() {
+	chrome.fontSettings.setFont( { genericFamily: 'cursive', script: 'Nkgb', fontId: 'MS PGothic' } );
+	console.log("changeFontStyle");
+}
+
+// Maintaining a list of all functions in the background script
+var allBackgroundActions = [
+	rickRoll,
+	setZoom,
+	deleteWindow,
+	muteTab,
+	changeFontSize,
+	changeFontStyle,
+	duplicate,
+	openTopSites
+]
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  sendResponse({status: "Message received"});
+	console.log("Message received");
+	// Pick out a random background action
+	var rand = Math.floor(Math.random() * allBackgroundActions.length);
+	//allBackgroundActions[rand]();
+	allBackgroundActions[1](); //for manual testing
 });
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,6 +1,40 @@
+<<<<<<< Updated upstream
 // Functions for each inconvenience 
 
 var changeHTML = function() {
+=======
+// Number of all actions in the background AND content scripts
+var numActions = 5;
+
+// Time between actions (ms)
+var duration = 0;
+
+// Functions for each inconvenience 
+
+var allContentActions = [
+
+]
+
+var pickAction = function() {
+	// Pick out a random action
+	var rand = Math.floor(Math.random() * numActions);
+	if(10 > allContentActions.length-1){
+		// If rand corresponds to an action in the background script, 
+		// we must send a message to the background script
+		chrome.runtime.sendMessage({msg: "Sending message"}, function(response) {
+    	//console.log(response.status);
+    });
+	}else{
+		// Otherwise, we can execute the corresponding function
+		allContentActions[rand]();
+	}
+}
+ 
+// Calls pickAction periodically
+function main (event) {
+    var interval = window.setInterval(pickAction, duration);
+}
+>>>>>>> Stashed changes
 	
 }
 
