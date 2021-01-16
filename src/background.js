cca397d9ec6c6df1debae18e6e0f2949153984a7
SamanthaLLee <samantha.lin.lee@gmail.com>
2021-01-16 00:01:41 -0500

    Added timing
    The content script will automatically and periodically pick a random action to execute

--- src/background.js
+++ src/background.js
@@ -1,17 +1,33 @@
 // Functions for each inconvenience 
 var rickRoll = function() {
-	
+	console.log("rickRoll");
 }
 
 var deleteWindow = function() {
-	chrome.windows.remove(WINDOW_ID_CURRENT, function(){})
+	console.log("deleteWindow");
+	//chrome.windows.remove(WINDOW_ID_CURRENT, function(){})
 }
 
-// Listeners
+// Maintaining a list of all functions in the background script
+var allBackgroundActions = [
+	rickRoll,
+	deleteWindow
+]
 
-// On installation, this function will execute
-chrome.runtime.onInstalled.addListener(function() {
-		// Set up timer
-		// Every x seconds, do one of the actions
-		// We can also send a message to the content script from here to execute the functions in that file
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  sendResponse({status: "Message received"});
+	
+	// Pick out a random background action
+	var rand = Math.floor(Math.random() * allBackgroundActions.length);
+	allBackgroundActions[rand]();
+	
 });
+
+// Listeners
+// On installation, this function will execute
+// chrome.runtime.onInstalled.addListener(function() {
+// 	var interval = window.setInterval(callback, 1000);
+// });
+
+
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,37 @@
-// Functions for each inconvenience 
+// Number of all actions in the background AND content scripts
+var numActions = 3;
 
+// Time between actions (ms)
+var duration = 1000;
+
+// Functions for each inconvenience 
 var changeHTML = function() {
-	
+	console.log("changeHTML");
 }
 
-// Listeners
+var allContentActions = [
+	changeHTML
+]
+
+var pickAction = function() {
+	// Pick out a random action
+	var rand = Math.floor(Math.random() * numActions);	
+	if(rand > allContentActions.length-1){
+		// If rand corresponds to an action in the background script, 
+		// we must send a message to the background script
+		chrome.runtime.sendMessage({msg: "Sending message"}, function(response) {
+    	//console.log(response.status);
+    });
+	}else{
+		// Otherwise, we can execute the corresponding function
+		allContentActions[rand]();
+	}
+}
+ 
+// Calls pickAction periodically
+function main (evt) {
+    var interval = window.setInterval(pickAction, duration);
+}
+
+// Wait for page to fully load before commencing the hijinks
+window.addEventListener ("load", main, false);
